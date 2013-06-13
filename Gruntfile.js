8c6de5a60a33bf487d6249dab5070220cc29a626
Nicolas Hery <nicolahery@gmail.com>
2013-06-13 12:37:53 +0200

    Update resume and dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,29 +5,47 @@ module.exports = function(grunt) {
 
     // Compile jade templates to html
     jade: {
-      // Use links to development assets
       html: {
-        src: ['index.jade'],
-        dest: '.',
         options: {
           client: false,
           pretty: true
+        },
+        files: {
+          'index.html': 'index.jade'
         }
       }
     },
 
     // Watch all source files and compile when changed
     watch: {
-      files: ['*.jade'],
-      tasks: 'jade'
+      html: {
+        files: ['*.jade'],
+        tasks: 'jade'
+      }
+    },
+
+    // Run development web server
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          port: 3000,
+          base: '.'
+        }
+      }
     }
 
   });
 
   // Load tasks from NPM packages
-  grunt.loadNpmTasks('grunt-jade'); // https://github.com/phated/grunt-jade
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task
   grunt.registerTask('default', 'jade');
+
+  // Alias connect task
+  grunt.registerTask('server', 'connect');
 
 };
--- README.md
+++ README.md
@@ -19,14 +19,18 @@ $ npm install
 Run:
 
 ```
-$ grunt --config Gruntfile.js watch
+$ grunt watch
 ```
 
 Change the `title` in `index.jade` to your name.
 
 Edit `resume.jade` using the [Jade](http://jade-lang.com/) templating language. I chose it for its short syntax, and because [Markdown](http://daringfireball.net/projects/markdown/) wasn't enough to make multiple columns, etc.
 
-If you fire up a server in the project directory you can see your changes in the browser as you save the `.jade` files.
+Fire up the server in the project directory you can see your changes in the browser as you save the `.jade` files:
+
+```
+$ grunt server
+```
 
 Edit `style.css` to your liking. Since resumes are meant to be printed, I defined font sizes and spacings using the `em` measure (versus `px`), that way I can change the default font size for the `@media print` query in order to make the resume fit on one page.
 
